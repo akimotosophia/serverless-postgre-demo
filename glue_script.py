3d40bce805bbe0797219dfdf08e76f616926010b
AtomikKeo <akimoto.sophia@gmail.com>
2025-08-05 00:12:16 +0900

    S3Import実装

--- glue_script.py
+++ glue_script.py
@@ -6,6 +6,8 @@ from awsglue.context import GlueContext
 from awsglue.dynamicframe import DynamicFrame
 from awsglue.job import Job
 from datetime import datetime
+import boto3
+import re
 
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 sc = SparkContext()
@@ -16,7 +18,11 @@ job.init(args['JOB_NAME'], args)
 
 run_id = datetime.now().strftime("%Y%m%d-%H%M%S")
 
-s3_output_path = f"s3://akimoto-serverless-etl/tmp/users_export/{run_id}"
+# S3からpartファイル名を探す
+bucket = "akimoto-serverless-etl"
+prefix = f"tmp/users_export/{run_id}"
+
+s3_output_path = f"s3://{bucket}/{prefix}"
 rds_iam_role = "arn:aws:iam::450347635113:role/s3-access-for-aurora-role"
 # Script generated for node PostgreSQL
 df = glueContext.create_dynamic_frame.from_options(
@@ -28,7 +34,7 @@ df = glueContext.create_dynamic_frame.from_options(
     connection_type="s3", 
     format="csv", 
     connection_options={
-        "paths": ["s3://akimoto-serverless-etl/users/"], 
+        "paths": [f"s3://{bucket}/users/"], 
         "recurse": True
     }, 
     transformation_ctx="read")
@@ -56,6 +62,8 @@ df =  DynamicFrame.fromDF(
 #    paths=["id", "username"],
 #    transformation_ctx="SelectFields_node1754063701594"
 #)
+df = df.coalesce(1)
+
 glueContext.write_dynamic_frame.from_options(
     frame=df,
     connection_type="s3",
@@ -67,8 +75,51 @@ glueContext.write_dynamic_frame.from_options(
     transformation_ctx="WriteToS3"
 )
 
+print("S3出力")
+print(df.toDF().count())
 
+s3 = boto3.client("s3")
+resp = s3.list_objects_v2(Bucket=bucket, Prefix=prefix)
+print("S3結果")
+print(resp)
+filename = resp["Contents"][0]["Key"]
 
+# conn_options = glueContext.extract_jdbc_conf("Aurora connection")
+
+# # AuroraにCOPY SQLを実行（psycopg2）
+# conn = psycopg2.connect(
+#     host=conn_options["fullUrl"],
+#     dbname="gluedb",
+#     user=conn_options["user"],
+#     password=conn_options["password"]
+#     )
+    
+# cur = conn.cursor()
+
+# sql = f"""
+# SELECT aws_s3.table_import_from_s3(
+#             'users',
+#             'username,email,first_name,last_name,birth_date',
+#             '(format csv, header)',
+#               aws_commons.create_s3_uri('{bucket}', '{filename}','ap-northeast-1')
+#             );
+# """
+
+# cur.execute(sql)
+# conn.commit()
+# cur.close()
+# conn.close()
+
+# df = df.toDF()
+# df.write \
+#     .format("jdbc") \
+#     .option("url", conn_options["fullUrl"]) \
+#     .option("dbtable", "users") \
+#     .option("user", conn_options["user"]) \
+#     .option("password", conn_options["password"]) \
+#     .option("batchsize", "100000") \
+#     .mode("append") \
+#     .save()
 glueContext.write_dynamic_frame.from_options(
     frame=empty_df,
     connection_type="postgresql",
@@ -81,7 +132,7 @@ glueContext.write_dynamic_frame.from_options(
             'users',
             'username,email,first_name,last_name,birth_date',
             '(format csv, header)',
-               aws_commons.create_s3_uri('akimoto-serverless-etl', 'run-1754136980092-part-r-00000.csv','ap-northeast-1')
+               aws_commons.create_s3_uri('{bucket}', '{filename}','ap-northeast-1')
             );
         """,
         "postactions": ""  # ログ記録等入れても良い
